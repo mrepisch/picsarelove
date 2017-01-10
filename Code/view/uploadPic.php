b88dbc4d3fef9e9bcab2573c6829c23c1bbbaf54
PetarB <petar.barisic2002@gmail.com>
2017-01-10 14:39:02 +0100

    Revert "Revert "Finished""
    This reverts commit dcd1bd2daafdc621fc25d81736a49517f028dc09.

--- Code/view/uploadPic.php
+++ Code/view/uploadPic.php
@@ -1,16 +1,15 @@
 
 <div id="content">
-	<form method="post">
+	<form method="post" action="index.php" enctype="multipart/form-data">
 		<input type="hidden" name="cont" value="Picture"></input>
 		<input type="hidden" name="action" value="upload">
 		<label for="title">Titel: </label> <input type="text" name="title"><br>
-		<label for="image">Bild hochladen: </label> <input type="file" name="image"><br>
+		<label for="picture">Bild hochladen: </label> <input type="file" name="picture" /><br>
 		<label for="category">Kategorie: </label>
 		<select name=category>
 			<option value="0" selected>Bitte Auswählen</option>
-		<?php foreach($entities as $entity) : ?>
-			
-		<?php endforeach; ?>
+			<option value="1" selected>Lustig</option>
+			<option value="2" selected>Cool</option>
 		</select><br>
 	   	<input type="submit" value="Hochladen" name="upload">
 	</form>
