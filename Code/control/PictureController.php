e4706dd9bb75650302869b4002b29b6e496aa69d
PetarB <petar.barisic2002@gmail.com>
2017-01-10 14:00:48 +0100

    Finished

--- Code/control/PictureController.php
+++ Code/control/PictureController.php
@@ -5,13 +5,20 @@ require_once 'model/PictureModel.php';
 class PictureController {
 	
 	function upload() {
+		$targetdir = '/pictures/';
+		$targetfile = $targetdir.$_FILES['picture']['name'];
+		
+		if (move_uploaded_file($_FILES['picture']['tmp_name'], $targetfile)) {
+		} else {
+		}
+		
 		$title = $_POST["title"];
-		$image = $_POST["image"];
 		$categoryID = $_POST["category"];
 		$userID = 1;
 		
 		$pictureModel = new PictureModel();
-		$pictureModel->createNewEntry($title, $image, $categoryID, $userID);
+		$pictureModel->createNewEntry($title, $targetfile, $categoryID, $userID);
+		header("Location:index.php");
 	}
 	
 	function displayForm() {
--- Code/model/PictureModel.php
+++ Code/model/PictureModel.php
@@ -4,15 +4,16 @@ require_once 'model/BaseModel.php';
 class PictureModel extends BaseModel {
 	
 	function __construct(){
-		BaseModel::__construct("picture","picID");		
+		BaseModel::__construct("pictures","picID");		
 	}
 	
 	function createNewEntry($p_title, $p_image, $p_categoryID, $p_userID) {
-		$query = "INSERT INTO $this->tableName (username, password, privileges ) VALUES (?, ?, ?);";
+		$query = "INSERT INTO $this->tableName (title, f_userID, f_categoryID, imagePath ) VALUES (?, ?, ?, ?);";
+		echo $query;
 		$conn = $this->connectToDb();
 		$statement = $conn->prepare($query);
 		
-		$statement->bind_param('sbii', $p_title, $p_image, $p_categoryID, $p_userID);
+		$statement->bind_param('ssss', $p_title, $p_userID, $p_categoryID, $p_image);
 		
 		if (!$statement->execute()) {
 			throw new Exception($statement->error);
